38f6458eefe53125b55deb27913d69cf0a2e570d
EnrikoAviyantoPutra <enriko.aviyanto@gmail.com>
2021-03-19 14:50:51 +0700

    Add Seeding Doctor

--- server/seeders/20210318152713-doctor.js
+++ server/seeders/20210318152713-doctor.js
@@ -2,23 +2,36 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Doctors', [
+      {
+        name: "Wiyono",
+        email: "wiyono.vet@test.com",
+        password: "123456789",
+        phoneNumber: "08999999912",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Budi",
+        email: "budi.vet@test.com",
+        password: "123123123",
+        phoneNumber: "08999999919",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Cantika",
+        email: "cantika.vet@test.com",
+        password: "987654321",
+        phoneNumber: "08888888881",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+
+    ])
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Doctors', null, {})
   }
 };
